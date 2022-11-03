943055c4ab1f1e3bc2263eb87c1bfdd218f8c1fe
shivam <gautamshivam25@gmail.com>
2022-11-03 15:28:30 -0500

    Trapping Rain Water

--- src/Q21/TrappingRainWater.java
+++ src/Q21/TrappingRainWater.java
@@ -1,6 +1,7 @@
 package Q21;
 
 /*
+ * https://leetcode.com/problems/trapping-rain-water/
  * Given n non-negative integers representing an elevation 
  * map where the width of each bar is 1, compute how much water it can trap after raining.
  * 
