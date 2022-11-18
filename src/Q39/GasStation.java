b77ef620add8b48605b1e29b4476ad37290b16ff
tantuwaySourabh <sourabhtan102@gmail.com>
2022-11-18 15:57:07 -0600

    gas station

---
+++ src/Q39/GasStation.java
@@ -0,0 +1,75 @@
+
+/**
+ * //Sourabh Tantuway, Shivam Gautam
+ * //SXT200011, SXG200011
+ * //Section: 001
+
+ PROBLEM STATEMENT: [Array]
+     There are n gas stations along a circular route, where the amount of gas at the ith station is gas[i].
+     You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from the ith station to its next (i + 1)th station.
+     You begin the journey with an empty tank at one of the gas stations.
+     Given two integer arrays gas and cost, return the starting gas station's index if you can travel around the circuit once in the clockwise direction, otherwise return -1.
+     If there exists a solution, it is guaranteed to be unique
+
+     Input: gas = [1,2,3,4,5], cost = [3,4,5,1,2]
+     Output: 3
+     Explanation:
+     Start at station 3 (index 3) and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
+     Travel to station 4. Your tank = 4 - 1 + 5 = 8
+     Travel to station 0. Your tank = 8 - 2 + 1 = 7
+     Travel to station 1. Your tank = 7 - 3 + 2 = 6
+     Travel to station 2. Your tank = 6 - 4 + 3 = 5
+     Travel to station 3. The cost is 5. Your gas is just enough to travel back to station 3.
+     Therefore, return 3 as the starting index.
+
+    link: https://leetcode.com/problems/gas-station/description/
+
+
+ **/
+
+package Q39;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GasStation {
+
+    public static int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalGas = 0;
+        int currentGas = 0;
+        int index = 0;
+
+
+        for(int i = 0; i < gas.length; i++){
+            currentGas += gas[i] - cost[i];
+            totalGas += gas[i] - cost[i];
+
+            if(currentGas < 0){
+                index = i + 1;
+                currentGas = 0;
+            }
+
+        }
+
+        return totalGas >= 0 ? index : -1;
+    }
+
+
+    public static void main(String[] args){
+        int[] gas = {1,2,3,4,5};
+        int[] cost = {3,4,5,1,2};
+        System.out.println("for arrays gas = [1,2,3,4,5], cost = [3,4,5,1,2] " );
+
+        System.out.println("station number(staring from 0) we can choose is : "  + canCompleteCircuit(gas,cost) );
+
+    }
+
+}
+
+//Complexity Analysis:
+/**
+ *
+ * TC : O(n) , linear time algorithm, one traversal.
+ * SC : O(1),
+ */
